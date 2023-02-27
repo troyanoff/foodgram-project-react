f5a77431528c5f1235fa4ac7e178022eb8fefeb2
troyanov <troyan0v@yandex.ru>
2023-02-27 23:12:37 +0300

    filters_v7

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -5,8 +5,14 @@ from recipes import models
 
 class RecipeFilter(filters.FilterSet):
 
-    is_favorited = filters.BooleanFilter(method='is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(method='is_in_shopping_cart')
+    is_favorited = filters.ModelChoiceFilter(
+        field_name='favorite_recipe',
+        method='is_favorited'
+    )
+    is_in_shopping_cart = filters.ModelChoiceFilter(
+        field_name='shopping',
+        method='is_in_shopping_cart'
+    )
     tags = filters.CharFilter(field_name='tags__slug', lookup_expr='icontains')
     author = filters.CharFilter(
         field_name='author__id', lookup_expr='icontains')
