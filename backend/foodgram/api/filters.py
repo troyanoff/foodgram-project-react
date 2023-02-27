d29e243598ce3dd5c7a8579dce57e76d6b20c66e
troyanov <troyan0v@yandex.ru>
2023-02-27 22:25:58 +0300

    filters_v5

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -8,7 +8,8 @@ class RecipeFilter(filters.FilterSet):
     is_favorited = filters.NumberFilter(method='is_favorited')
     is_in_shopping_cart = filters.NumberFilter(method='is_in_shopping_cart')
     tags = filters.CharFilter(field_name='tags__slug', lookup_expr='icontains')
-    tags = filters.CharFilter(field_name='author__id', lookup_expr='icontains')
+    author = filters.CharFilter(
+        field_name='author__id', lookup_expr='icontains')
 
     class Meta:
         model = models.Recipe
