d5b62371973155edb6546ed758416605ff7682df
troyanov <troyan0v@yandex.ru>
2023-02-27 22:08:36 +0300

    filters_v2

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -7,10 +7,12 @@ class RecipeFilter(filters.FilterSet):
 
     is_favorited = filters.NumberFilter(method='is_favorited')
     is_in_shopping_cart = filters.NumberFilter(method='is_in_shopping_cart')
+    tags = filters.CharFilter(field_name='tags__name', lookup_expr='icontains')
+    tags = filters.CharFilter(field_name='author__id', lookup_expr='icontains')
 
     class Meta:
         model = models.Recipe
-        fields = ('tags__name', 'author__id',)
+        fields = ('tags', 'author',)
 
     def is_favorited(self, queryset, name, value):
         if value:
